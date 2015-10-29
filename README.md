c0c4dc475394dda9edd40c797124670116ae7b6b
Marta Paciorkowska <marta@paciorkowska.pl>
2015-10-29 22:10:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# synapses
+
+Random snapshots of code whipped up together during my exploration of live coding in Clojure.
+
+## Usage
+
+Not much right now.
+
+## License
+
+Distributed under the Eclipse Public License version 1.0.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject synapses "0.1.0-SNAPSHOT"
+  :description "Attempts at live coding"
+  :url "http://github.com/xamebax/synapses"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                [overtone "0.8.1" :exclusions [org.clojure/clojure]]
+                [org.clojure/data.json "0.2.2"]])
